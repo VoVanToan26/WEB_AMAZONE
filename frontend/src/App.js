d8de1efd412ccfbf8909c9892b342acfe5b6cc2a
VoVanToan26 <99601581+VoVanToan26@users.noreply.github.com>
2022-11-06 23:06:37 +0700

    6.2 Implement Add To Cart Action

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,17 +1,29 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { BrowserRouter as Router, Link, Route, Routes } from 'react-router-dom';
 import { publicRoutes } from '~/routes';
+import { useSelector } from 'react-redux';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCartShopping } from '@fortawesome/free-solid-svg-icons';
 function App() {
+  // add count cart
+  const cart = useSelector((state) => state.cart);
+  const { cartItems } = cart;
+
   return (
     <Router>
       <div className="grid-container">
         <header className="row">
           <div>
-            <a className="brand" href="index.html">amazona</a>
+            <Link className="brand" to="./">amazona</Link>
           </div>
-          <div>
-            <a href="cart.html">Cart</a>
-            <a href="signin.html">Sign In</a>
+          <div className='Carrt' style={{position:'relative'}}>
+            <Link to="/cart">
+              Cart <FontAwesomeIcon style={{ 'fontSize': '2.5rem' }} icon={faCartShopping}/>
+              {cartItems.length > 0 && (
+                <span style={{ position: 'relative', top:'-1rem' }} className="badge">{cartItems.length}</span>
+              )}
+            </Link>
+            <Link to="/signin">Sign In</Link>
           </div>
         </header>
         <main>
--- frontend/src/Pages/Cart/CartPage.js
+++ frontend/src/Pages/Cart/CartPage.js
@@ -1,5 +1,7 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { useParams } from 'react-router-dom';
+import { useDispatch } from 'react-redux';
+import { addToCart } from '~/actions/cartActions';
 
 function CartPage() {
     const urlGetData = new URLSearchParams(window.location.search)
@@ -7,6 +9,13 @@ function CartPage() {
     console.log('props', props)
     const productId = props.id;
     const qty = urlGetData.get('qty')||1
+
+    const dispatch = useDispatch();
+    useEffect(() => {
+        if (productId) {
+            dispatch(addToCart(productId, qty));
+        }
+    }, [dispatch, productId, qty]);
     return (
         <div>
             <h1>Cart Screen</h1>
